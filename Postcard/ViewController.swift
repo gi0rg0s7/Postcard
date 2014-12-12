ea59dfdcbec31699f2bde184ed62a74a9fc7adb8
Giorgos Eleftheriou <giorgoseleftheriou@Giorgoss-MacBook-Pro.local>
2014-12-12 19:25:56 +0200

    first revof project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,29 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendMailButtonPressed(sender: AnyObject) {
+        // Make unhidden the hiden label
+        messageLabel.hidden = false
+        
+        //take the contet of messageLabel and add it to enterMessageTextField
+        messageLabel.text = enterMessageTextField.text
+        
+        //change the color of characters in messageLabel
+        messageLabel.textColor = UIColor.redColor()
+        
+        //after the button click the content of the enterMessage removed
+        enterMessageTextField.text = ""
+        
+        //the first responder in that case the keyboard to make it disappear
+        enterMessageTextField.resignFirstResponder()
+        
+        //change the name of the button after it pressed normal parameter can be change is depend when you want the button to be change for example if its highlighted etc
+        mailButton.setTitle("Mail Send", forState: UIControlState.Normal)
+        
+        
+        
     }
 
 
